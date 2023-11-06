2ecddd51aea8d4d1ab1ea891a8ba78251a728b0a
GabrielVictor0 <bielgvsa@gmail.com>
2023-11-06 10:55:53 -0300

    nav, menu lateral, header

--- eventplus/src/components/Nav/Nav.jsx
+++ eventplus/src/components/Nav/Nav.jsx
@@ -6,21 +6,25 @@ import logDesktop from '../../assets/images/logo-pink.svg'
 
 import {Link} from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({exibeNavbar, setExibeNavbar}) => {
+    console.log(`EXIBE O MENU? ${exibeNavbar}`);
     return (
-        <nav className='navbr'>
-            <span className='navbar__close'>x</span>
+        <nav className={`navbar ${exibeNavbar ? "exibeNavbar" : ""}`}>
+
+            <span onClick={() => {setExibeNavbar(false)}} className='navbar__close'>x</span>
+
             <Link to="/" className='eventlogo'>
                 <img className='eventlogo__logo-image' src={window.innerWidth >= 992 ? logDesktop : logMobile} alt="Event Plus Logo" />
             </Link>
 
             <div className="navbar__items-box">
-                <Link to="/">Home</Link>
-                <Link to="/tipo-eventos">Tipos de Evento</Link>
-                <Link to="/login">Usuario</Link>
-                <Link to="/eventos">Evento</Link>
-                <Link to="/testes">Teste</Link>
+                <Link to="/" className='navbar__item'>Home</Link>
+                <Link to="/tipo-eventos" className='navbar__item'>Tipos de Evento</Link>
+                <Link to="/login" className='navbar__item'>Usuario</Link>
+                <Link to="/eventos" className='navbar__item'>Evento</Link>
+                <Link to="/testes" className='navbar__item'>Teste</Link>
             </div>
+
         </nav>
     );
 };
