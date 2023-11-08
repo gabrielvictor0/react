35ddd1f00bee183325679784f29040e63346b8da
GabrielVictor0 <bielgvsa@gmail.com>
2023-11-08 10:53:25 -0300

    .

--- eventplus/src/pages/HomePage/HomePage.jsx
+++ eventplus/src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,37 @@ import './HomePage.css'
 import Banner from '../../components/Banner/Banner'
 import MainContent from '../../components/Main/MainContent';
 import VisionSection from '../../components/VisionSection/VisionSection';
+import ContactSection from '../../components/ContactSection/ContactSection'
+import Footer from '../../components/Footer/Footer'
+import Title from '../../components/Title/Title'
+import NextEvent from '../../components/NextEvent/NextEvent';
+import Container from '../../components/Container/Container';
 
 
 const HomePage = () => {
     return (
        <div>
            <MainContent>
-           <Banner/>
-           <VisionSection/>
+                <Banner/>
+                <section className='proximos-eventos'>
+                    <Container>
+                        <Title titleText={"Proximos Eventos"}/>
+
+                        <div className='events-box'>
+                            <NextEvent/>
+                            <NextEvent/>
+                            <NextEvent/>    
+                            <NextEvent/>    
+                            <NextEvent/>    
+                            <NextEvent/>    
+                            <NextEvent/>    
+                            <NextEvent/>    
+                        </div>
+                    </Container>
+                </section>
+                <VisionSection/>
+                <ContactSection/>
+                <Footer/>    
            </MainContent>
        </div>
     );
